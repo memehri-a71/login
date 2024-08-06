4473ebaba378102fa457c7ac85c9771f92d1a1c6
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-07 00:06:02 +0330

    debug httpservice

--- src/configs/httpService/httpService.ts
+++ src/configs/httpService/httpService.ts
@@ -1,21 +1,18 @@
 import { QueryClient } from "@tanstack/query-core";
 import axios from "axios";
 
-
-
 export const getQueryClient = new QueryClient();
 
- const axiosInstance = axios.create({
-  baseURL: `${process.env.BASE_URL}`,
-})
+//  const axiosInstance = axios.create({
+//   baseURL: `${process.env.BASE_URL}`,
+// })
 
 export const axiosGet = async ({ url }): Promise<any> => {
-  const { data } = await axiosInstance.get(url);
-  const results = data?.data;
-  return results;
+  const { data } = await axios.get(url);
+  return data;
 };
 
 export const axiosPost = async ({ url, body }): Promise<any> => {
-  const res = await axiosInstance.post(url, body);
+  const res = await axios.post(url, body);
   return res.data;
 };
