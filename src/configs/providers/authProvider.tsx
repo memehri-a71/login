3cfb25b2dc617a36488e099e22a75d47d614f00b
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-07 00:07:02 +0330

    debug auth provider

---
+++ src/configs/providers/authProvider.tsx
@@ -0,0 +1,12 @@
+import { AuthContext } from "../../context";
+import { useAuthContextValue } from "../../context/authContextValue";
+import { Children } from "../../types/common";
+
+export const AuthProvider = ({ children }: Children) => {
+  const { curStep, setCurStep } = useAuthContextValue();
+  return (
+    <AuthContext.Provider value={{ curStep, setCurStep }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
