3807e357476819ec3ad9f4c51741b9031a865cf0
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-06 19:20:34 +0330

    add formik to auth layout

--- src/components/authLayout.tsx
+++ src/components/authLayout.tsx
@@ -1,17 +1,22 @@
-import { ReactNode } from "react";
 import { CustomButton } from "./elements/customButton";
+import { FormikWrapper } from "./form/formikWrapper";
 
-interface AuthLayoutType {
-  children: ReactNode;
-}
-export const AuthLayout = ({ children }: AuthLayoutType) => {
+export const AuthLayout = (props: any) => {
+  const { initialValues, validationSchema, onSubmit, children } = props;
+
   return (
-    <div>
-      <div className="relative bg-[#0099a8] rounded-b-3xl h-[30vh]"></div>
-      <div className="absolute top-[10%] right-7 left-7 bg-white rounded-xl shadow-lg h-fit px-5 pb-10 pt-7">
-        {children}
-        <CustomButton>ادامه</CustomButton>
+    <FormikWrapper
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <div>
+        <div className="relative bg-[#0099a8] rounded-b-3xl h-[30vh]"></div>
+        <div className="absolute top-[10%] right-7 left-7 bg-white rounded-xl shadow-lg h-fit px-5 pb-10 pt-7">
+          {children}
+          <CustomButton >ادامه</CustomButton>
+        </div>
       </div>
-    </div>
+    </FormikWrapper>
   );
 };
