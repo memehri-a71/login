17c0a9124c10f27f4e41b58b605c41973da36f78
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-07 00:12:08 +0330

    create end points

---
+++ src/constants/endPoints.ts
@@ -0,0 +1,8 @@
+export const CREATE_OTP = "/create_otp";
+export const VALIDATE_OTP = "/validate_otp";
+export const PROVINCES_WOP = "https://stage-api.sanaap.co/base/provinces_wop";
+export const CHECK_AGENCY_CODE =
+  "https://stage-api.sanaap.co/api/v2/app/DEY/agent/verification/signup/check_agency_code";
+export const INSURANCE_BRANCH =
+  "ttps://stage-api.sanaap.co/api/v2/app/selection_item/insurance_branch/wop_list/";
+
