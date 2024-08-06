9612b8fafd63ea0564069347e5f49295287bfd1b
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-07 01:26:55 +0330

    login root

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,9 +1,13 @@
+import { useAuthContextValue } from "../../context/authContextValue";
+import { AgentInfo } from "../agentInfo";
+import { PhoneVerification } from "../phoneVerification";
+import { UserInfo } from "../useInfo";
 import { useLoginVeiwModel } from "./veiwModel";
 
 export const Login = (): any => {
   const { CurrentAuthComponent } = useLoginVeiwModel();
+  const { curStep } = useAuthContextValue();
 
-  return <CurrentAuthComponent />;
+
+  return <AgentInfo /> 
 };
-
-
--- src/views/login/veiwModel.tsx
+++ src/views/login/veiwModel.tsx
@@ -1,11 +1,17 @@
+import { useEffect } from "react";
+import { useAuthContextValue } from "../../context/authContextValue";
 import { PhoneVerification } from "../phoneVerification";
+import { UserInfo } from "../useInfo";
 
 export const useLoginVeiwModel = () => {
-  const curStep = "phone";
+  debugger;
+  const { curStep } = useAuthContextValue();
 
   const routeStepMapping = {
     phone: PhoneVerification,
+    userInfo: UserInfo,
   };
+
 
   const CurrentAuthComponent = routeStepMapping[curStep];
 
