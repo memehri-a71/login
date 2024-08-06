83e096c20d03253214557d6f190f09178b5572c4
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-06 13:35:03 +0330

    add react query config

---
+++ src/configs/providers/rqProvider.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+export default function RQProvider({
+  children,
+}: {
+  children: React.ReactNode
+}): React.ReactElement {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 3,
+      },
+    },
+  })
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
