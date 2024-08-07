6e14f4cb7bd6d7cc697953d5568c354efd12e021
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-07 23:10:38 +0330

    add RtlProvider

---
+++ src/configs/providers/rtlProvider.tsx
@@ -0,0 +1,17 @@
+import { prefixer } from 'stylis'
+import rtlPlugin from 'stylis-plugin-rtl'
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+
+export default function RtlProvider({
+  children,
+}: {
+  children: JSX.Element
+}): JSX.Element {
+  const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+  })
+
+  return <CacheProvider value={cacheRtl}>{children}</CacheProvider>
+}
