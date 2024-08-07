2774f2b22ac651cda099fc0ddb1f5acc75c7b6b2
mehri <m.ameri@mehrayan.com>
2024-08-07 09:39:11 +0330

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import "./App.css";
+import { CustomRadio } from "./components/elements/radio/customRadio";
 import { Providers } from "./configs/providers";
 import { Login } from "./views/login";
 
-
 function App() {
-
   return (
     <Providers>
       <div className="">
         <Login />
       </div>
-     </Providers>
+    </Providers>
   );
 }
 
