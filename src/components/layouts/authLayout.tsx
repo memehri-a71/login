1b0c63016524305e6dd841dcdb19676d183ab993
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-08 14:08:35 +0330

    add logo

--- src/components/layouts/authLayout.tsx
+++ src/components/layouts/authLayout.tsx
@@ -1,6 +1,6 @@
 import { CustomButton } from "../elements/button/customButton";
 import { FormikWrapper } from "../form/formikWrapper";
-
+import logo from "../../assets/logo.png";
 
 export const AuthLayout = (props: any) => {
   const { initialValues, validationSchema, onSubmit, children } = props;
@@ -14,6 +14,7 @@ export const AuthLayout = (props: any) => {
       <div>
         <div className="relative bg-primary-color rounded-b-3xl h-[30vh]"></div>
         <div className="absolute top-[10%] right-7 left-7 bg-white rounded-xl shadow-lg h-fit px-5 pb-10 pt-7">
+        <img src={logo} className="mx-auto w-15 h-10 mb-5"/>
           {children}
           <CustomButton>ادامه</CustomButton>
         </div>
