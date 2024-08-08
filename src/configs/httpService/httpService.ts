78fa29cd6faf51a84861baa506da1a2b3986266d
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-08 16:42:02 +0330

    add proxy to vite config for solving cors error

--- src/configs/httpService/httpService.ts
+++ src/configs/httpService/httpService.ts
@@ -1,46 +1,29 @@
-import { QueryClient } from "@tanstack/query-core";
-import axios from "axios";
-import { Login } from "../../views/login";
-import { showError } from "../../hook/useToust";
-
-export const getQueryClient = new QueryClient();
-
-// export const axiosInstance = axios.create({
-//   baseURL: "https://stage-api.sanaap.co",
-// });
-
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     return config;
-//   },
-//   (error) => {
-//     console.log(error);
-
-//     return Promise.reject(new Error("req interceptor error", error));
-//   }
-// );
-
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   async (error: any) => {
-//     console.log(error);
-//     showError(error);
-//     error.config.headers.Accept = "application/json";
-
-//     return Promise.reject(error);
-//   }
-// );
+import { axiosInstance } from "./interceptor";
 
 export const axiosGet = async ({ url }): Promise<any> => {
-  const { data } = await axios.get(url);
-  return data;
+  try {
+    const fullUrl = "https://stage-api.sanaap.co" + url;
+    const res = await axiosInstance.get(fullUrl);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
-export const axiosPost = async ({ url, body }): Promise<any> => {
-  const res = await axios.post(url, body, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
-  return res;
+export const axiosPost = async ({ url, body }) => {
+  try {
+    const fullUrl = "https://stage-api.sanaap.co" + url;
+
+    const res = await axiosInstance.post(fullUrl, body, {
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
@@ -7,10 +7,10 @@
 // })
 
 
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
@@ -18,5 +18,33 @@ export default defineConfig(({ mode }) => {
       'process.env.BASE_URL': JSON.stringify(env.BASE_URL)
     },
     plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://stage-api.sanaap.co',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
   }
+  
 })
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://stage-api.sanaap.co',
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// });
