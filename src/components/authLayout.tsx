7a622edf16a47f53a487fe76942350e1f3ca8b30
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-08 11:26:47 +0330

    debugging

--- src/components/authLayout.tsx
+++ src/components/authLayout.tsx
@@ -11,10 +11,10 @@ export const AuthLayout = (props: any) => {
       onSubmit={onSubmit}
     >
       <div>
-        <div className="relative bg-[#0099a8] rounded-b-3xl h-[30vh]"></div>
+        <div className="relative bg-primary-color rounded-b-3xl h-[30vh]"></div>
         <div className="absolute top-[10%] right-7 left-7 bg-white rounded-xl shadow-lg h-fit px-5 pb-10 pt-7">
           {children}
-          <CustomButton >ادامه</CustomButton>
+          <CustomButton>ادامه</CustomButton>
         </div>
       </div>
     </FormikWrapper>
--- src/components/elements/customButton.tsx
+++ src/components/elements/customButton.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@mui/material";
 
 export const CustomButton = (props: any) => {
-  const { children, type = "submit", variant = "contained", onClick } = props;
+  const {
+    children,
+    type = "submit",
+    variant = "contained",
+    onClick,
+    color = "primary",
+  } = props;
   return (
     <Button
       type={type}
       variant={variant}
       fullWidth
-      color="success"
+      color={color}
       onClick={onClick}
       disableRipple
     >
