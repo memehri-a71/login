d00255d26e7f78fee46e52698e0f9c822ac3597a
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-08 16:43:03 +0330

    add loading to component

--- src/views/login/otpVerification/index.tsx
+++ src/views/login/otpVerification/index.tsx
@@ -3,14 +3,15 @@ import { AuthLayout } from "../../../components/layouts/authLayout";
 import { OtpVerificationView } from "./view";
 import { useOtpVerificationViewModel } from "./viewModel";
 
-export const OtpVerification = () => {
-  const { validationSchema, initialValues, onSubmit, resendCode } =
-    useOtpVerificationViewModel();
+export const OtpVerification = ({setCurStep}) => {
+  const { validationSchema, initialValues, onSubmit, resendCode ,isPending} =
+    useOtpVerificationViewModel(setCurStep);
 
   return (
     <AuthLayout
       initialValues={initialValues}
       validationSchema={validationSchema}
+      isLoading={isPending}
       onSubmit={onSubmit}
     >
       <OtpVerificationView resendCode={resendCode} />
--- src/views/login/phoneVerification/index.tsx
+++ src/views/login/phoneVerification/index.tsx
@@ -4,13 +4,14 @@ import { AuthLayout } from "../../../components/layouts/authLayout";
 import { PhoneVerificationView } from "./view";
 import { usePhoneVerificationViewModel } from "./viewModel";
 
-export const PhoneVerification = () => {
-  const { initialValues, validationSchema, onSubmit } =
-    usePhoneVerificationViewModel();
+export const PhoneVerification = ({setCurStep}) => {
+  const { initialValues, validationSchema, onSubmit,isPending } =
+    usePhoneVerificationViewModel(setCurStep);
   return (
     <AuthLayout
       initialValues={initialValues}
       validationSchema={validationSchema}
+      isLoading={isPending}
       onSubmit={onSubmit}
     >
       <PhoneVerificationView />
