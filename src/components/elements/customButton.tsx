ee60066a377dde3d91763cc80246a84eef5074a3
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-08 09:16:07 +0330

    debugging custom button

--- src/components/elements/customButton.tsx
+++ src/components/elements/customButton.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@mui/material";
 
-export const CustomButton = (props:any) => {
-  const { children, type = "submit" } = props;
+export const CustomButton = (props: any) => {
+  const { children, type = "submit", variant = "contained", onClick } = props;
   return (
-    <Button type={type} variant="contained" fullWidth color="success">
+    <Button
+      type={type}
+      variant={variant}
+      fullWidth
+      color="success"
+      onClick={onClick}
+      disableRipple
+    >
       {children}
     </Button>
   );
