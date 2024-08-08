34c9f50300ba7bf53cb18d92132e23313bbc9247
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-08 09:16:36 +0330

    create OtpCountdownTimer component

---
+++ src/components/elements/counter/otpCountdownTimer.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { CustomButton } from "../customButton";
+import CountDownTimer from "./countDownTimer";
+
+
+export const OtpCountdownTimer = ({
+  handleResendOtp,
+  isLoadingResend,
+  defaultTimer,
+}) => {
+  const OTP_EXPIRE_TIME = 5;
+  const [timer, setTimer] = useState(defaultTimer || OTP_EXPIRE_TIME);
+
+  useEffect(() => {
+    if (!isLoadingResend && timer === 0) {
+      setTimer(defaultTimer || OTP_EXPIRE_TIME);
+    }
+  }, [isLoadingResend]);
+
+  return (
+    <div className="flex gap-4 items-center">
+      {timer === 0 ? (
+        <CustomButton
+          variant="text"
+          type="button"
+          onClick={() => {
+            handleResendOtp();
+          }}
+        >
+          <p>ارسال مجدد کد</p>
+          {/* {isLoadingResend ? <Spinner /> : null} */}
+        </CustomButton>
+      ) : (
+        <CountDownTimer timer={timer} setTimer={setTimer} />
+      )}
+    </div>
+  );
+};
+
+
