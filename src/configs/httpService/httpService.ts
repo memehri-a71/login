8dc240f9e889e4964341e627e0c99eefb8e1fbc4
memehri-a71 <mehri.ameri.92@gmail.com>
2024-08-08 11:23:59 +0330

    add interceptor axios

--- src/configs/httpService/httpService.ts
+++ src/configs/httpService/httpService.ts
@@ -1,31 +1,48 @@
 import { QueryClient } from "@tanstack/query-core";
 import axios from "axios";
+import { Login } from "../../views/login";
 
 export const getQueryClient = new QueryClient();
 
-//  const axiosInstance = axios.create({
-//   baseURL: `${process.env.BASE_URL}`,
-// })
+export const axiosInstance = axios.create({
+  baseURL: "https://stage-api.sanaap.co",
+});
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log(error);
+
+    return Promise.reject(new Error("req interceptor error", error));
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error: any) => {
+    console.log(error);
+
+    error.config.headers.Accept = "application/json";
+
+    return Promise.reject(error);
+  }
+);
 export const axiosGet = async ({ url }): Promise<any> => {
-  const { data } = await axios.get(url);
+  const { data } = await axiosInstance.get(url);
   return data;
 };
 
 export const axiosPost = async ({ url, body }): Promise<any> => {
-  const res = await axios.post(
-    url,
-    body
-    //    {
-    //   headers: {
-    //    'Content-Type': 'application/json',
-
-    //       'Access-Control-Allow-Methods': 'OPTIONS,POST',
-    //       'Access-Control-Allow-Credentials': true,
-    //       'Access-Control-Allow-Origin': '*',
-    //       'X-Requested-With': '*',
-    //   },
-    // }
-  );
+  const res = await axiosInstance.post(url, body, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "OPTIONS,POST",
+      "Access-Control-Allow-Credentials": 'true',
+      "Access-Control-Allow-Origin": "*",
+      "X-Requested-With": "*",
+    },
+  });
   return res.data;
 };
